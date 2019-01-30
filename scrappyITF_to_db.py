286df25af8db3c76ddfe30a18aa46053921c984c
Omair <omair@khan.com>
2019-01-30 20:55:32 +0530

    Tree structure in json

--- scrappyITF_to_db.py
+++ scrappyITF_to_db.py
@@ -1,6 +1,7 @@
 import re
 import time
 import os
+import pprint
 
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
@@ -8,8 +9,8 @@ from pymongo import MongoClient
 dir = os.getcwd()
 
 client = MongoClient('mongodb://localhost:27017') #port 27017
-db = client.torna_database
-print(db)
+# db = client.torna_database
+
 soup = BeautifulSoup(open("{}/itf.html".format(dir)),"html.parser")
 th = soup.find_all('th')
 
@@ -38,43 +39,33 @@ for i in range(1,251):
     place=re.sub(r'\n*\n',r'',tr[i].td.find_next_sibling().text,flags=re.M)
     date=re.sub(r'\n*\n',r'',tr[i].td.find_next_sibling().find_next_sibling().text,flags=re.M)
     grade=re.sub(r'\n*\n',r'',tr[i].td.find_next_sibling().find_next_sibling().find_next_sibling().text,flags=re.M)
+    
+    startDate,endDate = date.split("-")
+    startDateDay,startDateMonth = startDate.split()
+    endDateDay,endDateMonth,Year = endDate.split()
+
+    
     Tournament = {"name": "{}".format(name),
                   "place": "{}".format(place),
-                  "date": date,
+                  "date": "{}".format(date),
+                  "start date": [{
+                      "day": "{}".format(startDateDay),
+                      "month": "{}".format(startDateMonth)}],
+                  
+                  "end date": "{}".format(endDate),
+                  
+                  "start date": [{
+                      "day": "{}".format(endDateDay),
+                      "month": "{}".format(endDateMonth)}],
+                  "year": "{}".format(Year),
+                
+                  
                   "grade": "{}".format(grade)}
     posts = db.posts
     post_id = posts.insert_one(Tournament).inserted_id
 
         
-    # DATE=Node("DATE",date="{}".format(date))
-    # GRADE=Node("GRADE",grade="{}".format(grade))
-    # PLACE=Node("PLACE",place="{}".format(place))
-
-    # date_search = list(graph.find("DATE", property_key ='date',property_value = date))
-    # grade_search = list(graph.find("GRADE", property_key ='grade',property_value = grade))
-    # place_search = list(graph.find("PLACE", property_key ='place',property_value = place))
-    # if len(date_search) > 0:
-    #     DATE = matcher.match("DATE",date="{}".format(date))
-    # else:
-    #     graph.create(DATE)
-
-    # if len(grade_search) > 0:
-    #     GRADE = matcher.match("GRADE",grade="{}".format(grade))
-
-    # else:
-    #     graph.create(GRADE)
-
-
-    # if len(place_search) > 0:
-    #     PLACE = matcher.match("PLACE",place="{}".format(place))
-
-    # else:
-    #     graph.create(PLACE)
-
-    # TOURNAMENT = Node("TOURNAMENT",tourna = "{}".format(name))
-    # TOURNAMENT_date = Relationship(TOURNAMENT,"is on",DATE)
-    # TOURNAMENT_place = Relationship(TOURNAMENT,"is at",PLACE)
-    # TOURNAMENT_grade = Relationship(TOURNAMENT,"is of grade",GRADE)
+    
 
 
 
@@ -96,14 +87,31 @@ for i in range(1,95):
     date=re.sub(r'\n*\n',r'',tr[i].td.find_next_sibling().find_next_sibling().text,flags=re.M)
     grade=re.sub(r'\n*\n',r'',tr[i].td.find_next_sibling().find_next_sibling().find_next_sibling().text,flags=re.M)
     
+    startDate,endDate = date.split("-")
+    startDateDay,startDateMonth = startDate.split()
+    endDateDay,endDateMonth,Year = endDate.split()
+
+    
     Tournament = {"name": "{}".format(name),
                   "place": "{}".format(place),
-                  "date": date,
+                  "date": "{}".format(date),
+                  "start date": [{
+                      "day": "{}".format(startDateDay),
+                      "month": "{}".format(startDateMonth)}],
+                  
+                  "end date": "{}".format(endDate),
+                  
+                  "start date": [{
+                      "day": "{}".format(endDateDay),
+                      "month": "{}".format(endDateMonth)}],
+                  "year": "{}".format(Year),
+                
+                  
                   "grade": "{}".format(grade)}
-    posts = db.posts
-    post_id = posts.insert_one(Tournament).inserted_id
     
-    # Tournament=Node("Tournament",name="{}".format(name),place="{}".format(place),date="{}".format(date),grade="{}".format(grade))
-    # graph.create(Tournament)
+    
 
 db.collection_names(include_system_collections=False)
+for post in posts.find():
+
+    pprint.pprint(post)
